211fcef6fb36cf0e521106a6e25246191e492fb7
agripe049 <agripeheber@gmail.com>
2025-12-23 12:39:48 -0300

    Ajustando Css, na página de Filme(Detalhes)

--- src/pages/Filme/Filme.jsx
+++ src/pages/Filme/Filme.jsx
@@ -11,12 +11,10 @@ function Filme() {
   useEffect(() => {
     async function carregarFilme() {
       const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}&language=pt-BR`;
-
       const response = await fetch(url);
       const data = await response.json();
 
       setFilme(data);
-      
     }
     carregarFilme();
   }, [id]);
@@ -24,16 +22,23 @@ function Filme() {
   if (!filme) return <p>Carregando...</p>;
   const ano = filme.release_date?.slice(0, 4);
 
+  function formatarDuracao(minutos) {
+    const h = Math.floor(minutos / 60);
+    const m = minutos % 60;
+    return `${h}h ${m}min`;
+  }
+
   return (
     <div className='filme-container'>
       <img
-        src={`https://image.tmdb.org/t/p/w200${filme.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
         alt={filme.title}
         className='filme-poster'
       /> 
       <div className='filme-info'>
         <h1>{filme.title}</h1>
         {ano && <span>{ano}</span>}
+        <p>{formatarDuracao(filme.runtime)}</p>
         <p>{filme.overview}</p>
 
         <Link to="/">
